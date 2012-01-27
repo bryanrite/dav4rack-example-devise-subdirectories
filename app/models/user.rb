2bdde8dbf5fa3c04998d80e05631e4448fe77058
Bryan Rite <bryan@bryanrite.com>
2012-01-27 15:09:45 -0800

    setup devise with username instead of email, create user subdirectory upon registraion

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,14 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :username, :email, :password, :password_confirmation, :remember_me
+  
+  after_create :create_userfile_directory
+  
+  validates :username, presence: true
+  
+  def create_userfile_directory
+    FileUtils.mkdir(File.join(Rails.root, 'public', 'system', 'userfiles', id.to_s))
+  end
+  
 end
